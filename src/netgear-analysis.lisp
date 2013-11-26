abb3fb2d3d56102b88418aab6572ae9a90ee4243
Eric Schulte <schulte.eric@gmail.com>
2013-11-25 21:08:05 -0700

    full complement of first round runs

--- src/netgear-analysis.lisp
+++ src/netgear-analysis.lisp
@@ -5,12 +5,16 @@
 (in-package :netgear-repair)
 
 (defvar 1-fix-paths (list
+                     "checkpoints/interactive/1-0/90405-fixes.store"
                      "checkpoints/interactive/1-1/17231-fixes.store"
                      "checkpoints/interactive/1-2/26879-fixes.store"
                      "checkpoints/interactive/1-3/23764-fixes.store"
                      "checkpoints/interactive/1-4/47906-fixes.store"
                      "checkpoints/interactive/1-5/13102-fixes.store"
-                     "checkpoints/interactive/1-maybe/20677-fixes.store")
+                     "checkpoints/interactive/1-6/76960-fixes.store"
+                     "checkpoints/interactive/1-7/11831-fixes.store"
+                     "checkpoints/interactive/1-8/2846-fixes.store"
+                     "checkpoints/interactive/1-9/25600-fixes.store")
   "Paths to evolved fixes found using only the exploit tests.")
 
 (defvar 1-fix-evals
@@ -18,19 +22,20 @@
             (multiple-value-bind (matched-p matches)
                 (scan-to-strings "/([0-9]+)-fixes" path)
               (parse-integer (aref matches 0))))
-          1-fix-paths))     ; => (17231 26879 23764 47906 13102 20677)
+          1-fix-paths))
+;; => (90405 17231 26879 23764 47906 13102 76960 11831 2846 25600)
 
-(defvar 1-bests (mapcar [#'lastcar #'restore] 1-fix-paths)
+(defvar 1-evolved (mapcar [#'lastcar #'restore] 1-fix-paths)
   "Un-minimized evolved fixes found using only the exploit tests.")
+;; => (store 1-evolved "stuff/1-evolved.store")
 
-(defvar 1-full-fitnesses (let ((tests nil)) (mapcar #'test 1-bests))
-  "Fitness of `1-bests' using the full regression tests suite.")
-                                        ; => (22 21 19 6 16 18)
+(defvar 1-full-fitnesses (let ((tests nil)) (mapcar #'test 1-evolved))
+  "Fitness of `1-evolved' using the full regression tests suite.")
+;; => (8 22 21 19 6 16 17 20 14 21)
 
-(defvar 1-minimized (mapcar {delta-debug orig} 1-bests)
+(defvar 1-minimized (mapcar {delta-debug orig} 1-evolved)
   "Minimized evolved fixes found using only the exploit tests.")
-;; (store 1-minimized "checkpoints/interactive/1-1-6-minmized.store")
+;; => (store 1-minimized "stuff/1-minmized.store")
 
 (defvar 1-full-min-fitnesses (let ((tests nil)) (mapcar #'test 1-minimized))
   "Fitness of `1-minimized' using the full regression tests suite.")
-                                        ; => (22 22 22 6 22 22)
